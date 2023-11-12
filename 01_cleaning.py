419ed36c67d18964936bf24cb4d3e14d13f4acc3
hyperforest <rimbaerlangga@gmail.com>
2023-11-12 08:53:56 +0700

    fix cleaning

--- 01_cleaning.py
+++ 01_cleaning.py
@@ -1,15 +1,17 @@
-import os
 import re
-import duckdb
+import json
 import numpy as np
 import pandas as pd
 
-from fuzzywuzzy import fuzz
-from tqdm import tqdm
+from time import time
 
 
 def is_containing_non_alphanumeric(string):
-    return not bool(re.search(r'[^a-zA-Z0-9 ]', string))
+    return not bool(re.search(r"[^a-zA-Z0-9 ]", string))
+
+
+def is_alphabet_only(string):
+    return string.isalpha()
 
 
 def clean(s):
@@ -17,70 +19,128 @@ def clean(s):
 
     # give spaces before and after the special characters, that are not number or decimal number
     # e.g. 'abc+c' to 'abc + c', but '12.9' not to '12 . 9'
-    s = re.sub(r'([^\d\.,-]+)', r' \1 ', s)
+    s = re.sub(r"([\@\-\"\'])", r" \1 ", s)
+
+    # remove parentheses
+    s = re.sub(r"(\(|\))", r"", s)
+
+    # set non alphabets, non numbers, and non punctuations to give spaces sign
+    s = re.sub(r"([^a-zA-Z0-9\.\,\+\-\ ]+)", r" \1 ", s)
+
+    # give another spaces
+    s = re.sub(r"(\+|,)", r" \1 ", s)
+    s = re.sub(r"([a-z]+)(\.)", r"\1 \2", s)
+    s = re.sub(r"([a-zA-Z]+)([0-9]+)", r"\1 \2", s)
 
     # separate value and units like "100ml" to "100 ml" or "50g" to "50 g"
     # that are not wrapped in <...> bracket
-    s = re.sub(r'(\d+)([a-zA-Z]+)', r'\1 \2', s)
+    s = re.sub(r"(\d+)([a-zA-Z]+)", r"\1 \2", s)
 
     # make chemical formula separator uniform to 'x', i.e. '4.5 - 3.6 - 2.1' to '4.5x3.6x2.1'
-    s = re.sub(r'(\d+\.?,?\d*)[×x\+,\. |-]+(\d+\.?,?\d*)[×x\+,\. |-]+(\d+\.?,?\d*)', r'\1x\2x\3', s)
+    s = re.sub(
+        r"(\d+\.?,?\d*)[×x\+,\. |-]+(\d+\.?,?\d*)[×x\+,\. |-]+(\d+\.?,?\d*)",
+        r"\1x\2x\3",
+        s,
+    )
 
-    # remove extra spaces
-    s = re.sub(r'\s+', ' ', s)
-    
+    # separate spaces from chemical formula
+    s = re.sub(r"(\d+)(\+|-)", r"\1 +", s)
+    s = re.sub(r"(\+|-)(\d+) |\+([a-zA-Z]+)", r"\1 \2 \3", s)
+    s = re.sub(r"([a-zA-Z]+)(\+|-)", r"\1 \2", s)
+    s = re.sub(r"(\+|-)([0-9]+)", r"\1 \2", s)
+
+    # separate comma from alphabets
+    s = re.sub(r"([a-zA-Z]+),([a-zA-Z]+)", r"\1 , \2", s)
+
+    s = re.sub(r"\s+", " ", s)  # remove extra spaces once again
+
     return s
 
 
-def main():
-    df_name = pd.read_excel('datasets/raw/product_name.xlsx')
-    catalog = pd.read_excel('datasets/raw/product_catalog.xlsx')
+def extract_non_formula(text):
+    pattern = r"^[^\d]+"
+    match = re.search(pattern, text)
 
-    df_name.rename(columns={'Product Name': 'product_name'}, inplace=True)
-    catalog.rename(columns={
-        'Product SKU': 'product_sku',
-        'Brand': 'brand',
-        'Type': 'type',
-        'Formula': 'formula'
-    }, inplace=True)
+    if match:
+        extracted_prefix = match.group()
+        return extracted_prefix.strip()
+
+    return np.nan
+
+
+def extract_formula(s):
+    match = re.search(r"\d+\.?,?\d*x\d+\.?,?\d*x+\d+\.?,?\d*", s)
+    if match:
+        return match.group()
+    return np.nan
+
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = time()
+        res = func(*args, **kwargs)
+        end = time()
+        print(f"Time taken: {end - start:.2f} seconds")
+        return res
+
+    return wrapper
+
+
+@timeit
+def main():
+    df_name = pd.read_excel("datasets/raw/product_name.xlsx")
+    catalog = pd.read_excel("datasets/raw/product_catalog.xlsx")
+
+    df_name = df_name.reset_index().rename(columns={"index": "product_id"})
+    catalog = catalog.reset_index().rename(columns={"index": "sku_id"})
+
+    df_name.rename(columns={"Product Name": "product_name"}, inplace=True)
+    catalog.rename(
+        columns={
+            "Product SKU": "product_sku",
+            "Brand": "brand",
+            "Type": "type",
+            "Formula": "formula",
+        },
+        inplace=True,
+    )
 
     df_name = df_name.dropna()
 
-    df_name['is_only_alphanum'] = df_name['product_name'].apply(is_containing_non_alphanumeric)
-    catalog['is_only_alphanum'] = catalog['product_sku'].apply(is_containing_non_alphanumeric)
+    df_name["is_name_only_alphanum"] = df_name["product_name"].apply(
+        is_containing_non_alphanumeric
+    )
+    df_name["is_name_only_alphabet"] = df_name["product_name"].apply(is_alphabet_only)
+    df_name["clean_name"] = df_name.product_name.apply(clean)
+    df_name["clean_name_non_formula"] = df_name.clean_name.apply(extract_non_formula)
+    df_name["clean_name_formula"] = df_name.clean_name.apply(extract_formula)
 
-    catalog['clean_sku'] = catalog.product_sku.apply(clean)
-    df_name['clean_name'] = df_name.product_name.apply(clean)
-    catalog['brand'] = catalog.brand.str.lower().str.replace(' ', '')
+    catalog["is_sku_only_alphanum"] = catalog["product_sku"].apply(
+        is_containing_non_alphanumeric
+    )
+    catalog["is_sku_only_alphabet"] = catalog["product_sku"].apply(is_alphabet_only)
+    catalog["clean_sku"] = catalog.product_sku.apply(clean)
+    catalog["clean_sku_non_formula"] = catalog.clean_sku.apply(extract_non_formula)
+    catalog["clean_sku_formula"] = catalog.clean_sku.apply(extract_formula)
+    
+    catalog["brand"] = catalog.brand.str.lower().str.replace("/", " ")
 
     # tag possible brand
     unique_brand = sorted(catalog.brand.unique())
-    brand_sku_tokens = {}
+    with open("common_tokens.json", "r", encoding='utf-8') as f:
+        brand_sku_tokens = json.load(f)
 
+    df_name['possible_brand'] = None 
     for brand in unique_brand:
-        brand_sku_tokens[brand] = set()
+        pattern = brand_sku_tokens[brand]
+        df_name.loc[
+            df_name.clean_name.str.replace(" ", "").str.contains(pattern),
+            'possible_brand'
+        ] = brand
 
-        for sku in catalog[catalog.brand == brand].clean_sku:
-            brand_sku_tokens[brand] |= set(sku.split(' '))
+    df_name.to_csv("datasets/processed/product_name.tsv", sep="\t", index=False)
+    catalog.to_csv("datasets/processed/product_catalog.tsv", sep="\t", index=False)
 
-    for brand in brand_sku_tokens:
-        # keep alphabetics with length >= 4 only
-        brand_sku_tokens[brand] = set(filter(lambda x: len(x) >= 4 and x.isalpha(), brand_sku_tokens[brand]))
 
-    brand_sku_tokens['dgw/hextar'] |= {'hx', 'dgw'}
-    # brand_sku_tokens
-
-    def possible_brands(string):
-        for brand in unique_brand:
-            for token in brand_sku_tokens[brand]:
-                if token in string:
-                    return brand
-        return np.nan
-
-    df_name['possible_brand'] = df_name.clean_name.str.replace(' ', '').apply(possible_brands)
-
-    df_name.to_csv('datasets/processed/product_name.tsv', sep='\t', index=False)
-    catalog.to_csv('datasets/processed/product_catalog.tsv', sep='\t', index=False)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
